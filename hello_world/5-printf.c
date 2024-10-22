54cc9beee085691535565970258087bbfdb5794e
root <root@Lucawin.>
2024-10-22 11:00:27 +0200

    mamacitouche

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,18 @@
+/*
+ * File: 5-printf.c
+ */
 
 #include <stdio.h>
 
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," .
+ *
+ * Return: stope 0.
+ */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
