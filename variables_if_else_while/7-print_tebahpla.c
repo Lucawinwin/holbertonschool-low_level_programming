f5d33a361bdf072d644d9352028dadfb9dccad78
root <root@Lucawin.>
2024-10-23 15:02:26 +0200

    bella ciao

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - imprime l'alphababet à l'envers 
+ *
+ * Return: Toujours 0 (Sucess)
+
+int main(void)
+{
+char ch;
+
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
