a94191abbdbcc9e28b0a6f9d4df39cfaa4f0d59d
root <root@Lucawin.>
2024-10-23 10:25:13 +0200

    all i do is win

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,21 +4,24 @@
 
 int main(void)
 {
-int:(int a, int b) 
-}
+int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+
+/* Your code starts here */
 if (n > 0)
 {
-printf("%d is positive\n", n)
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
 printf("%d is zero\n", n);
 }
 else
-{
 printf("%d is negative\n", n);
 }
+    /* Your code ends here */
+
 return (0);
 }
