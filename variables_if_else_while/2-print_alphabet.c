b26f1980fc0609c5ed4834b8b4d11d77d6d5dbd1
root <root@Lucawin.>
2024-10-23 13:50:32 +0200

    au dd

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
+#include <unistd.h>
 
-int main(void) {
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++) {
-putchar(ch);
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
 }
 putchar('\n');
-return 0;
+
+return (0);
 }
