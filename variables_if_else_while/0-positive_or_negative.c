929be4484cd31c7a8be869ce34babfbdb6110609
root <root@Lucawin.>
2024-10-23 10:51:17 +0200

    rt

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - entry point
+ *
+ * Return always 0 (Sucess)
+
 int main() {
     srand(time(NULL)); // Initialiser le générateur de nombres aléatoires
     int n = rand() % (RAND_MAX + 1) - (RAND_MAX / 2); // Générer un nombre aléatoire entre -RAND_MAX/2 et RAND_MAX/2
