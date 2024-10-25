4a012046984aceff6e94b77034158225af9947fd
root <root@Lucawin.>
2024-10-25 10:13:12 +0200

    rty

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
-
+#include <unistd.h>
+/**
+ *main- rend et imprime
+ *les nombres de 0 a 9
+ *et de a a f
+ *Return: always 0 (sucess)
+ */
 int main(void)
 {
-char hex_digits[] = "0123456789abcdef";
-for (int i = 0; i < 16; i++)
-{
-putchar(hex_digits[i]);
-}
-putchar('\n');
-return 0;
-}
+char ab;
+char cd;
 
+for (ab = '0'; ab <= '9'; ab++)
+putchar(ab);
+for (cd = 'a'; cd <= 'f'; cd++)
+putchar(cd);
+
+putchar ('\n');
+
+return (0);
+}
