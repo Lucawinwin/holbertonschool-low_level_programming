3ad4aea1a2983e08635bb3e8e5f73d1b6e47be7a
root <root@Lucawin.>
2024-10-25 11:11:29 +0200

    rtg

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
 
-int main(void)
-{
-printf("_putchar\n");
-return (0);
+int main(void) {
+char str[] = "_putchar";
+write(1, str, 8);  // Écrit la chaîne sur la sortie standard (descripteur de fichier 1)
+write(1, "\n", 1); // Écrit un caractère de nouvelle ligne
+
+return 0;
 }
 
