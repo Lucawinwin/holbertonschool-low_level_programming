071d47ede3bf519f118ea2a4077452fa2d920c96
root <root@Lucawin.>
2024-10-28 17:04:03 +0100

    sdfdf

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,34 +1,22 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * imprime nombre de 0 a 9
  */
-int _putchar(char c) {
-return putchar(c); // Using putchar from stdio.h for simplicity
-}
 
-/**
- * print_numbers - prints the numbers from 0 to 9, followed by a new line
- */
+int _putchar (char c) {
+return (putchar) (c)}
+
 void print_numbers(void)
 {
-char numbers[10] = "0123456789"; // String containing numbers from 0 to 9
-for (int i = 0; i < 10; i++)
+char [10] = "0123456789";
+
+for (int i = 0; i < 10 ; 1++)
 {
 _putchar(numbers[i]);
 }
-    
+
 _putchar('\n');
-}
 
-
-int main(void)
-{
-print_numbers();
-return 0;
-}
-i
+return (0);
