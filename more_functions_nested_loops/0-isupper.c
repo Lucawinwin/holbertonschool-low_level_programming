883d1307735f1dd616e50f15a9fca599c65ad3b0
root <root@Lucawin.>
2024-10-28 14:19:49 +0100

    chouchou

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -8,28 +8,28 @@
  */
 int _isupper(int c)
 {
-// Check if c is between 'A' and 'Z'
-if (c >= 'A' && c <= 'Z')
-{
-return 1; // c is uppercase
-}
-return 0; // c is not uppercase
+    // Check if c is between 'A' and 'Z'
+    if (c >= 'A' && c <= 'Z')
+    {
+        return 1; // c is uppercase
+    }
+    return 0; // c is not uppercase
 }
 
 // Example usage
 int main(void)
 {
-char test_char = 'A';
+    char test_char = 'A';
     
-if (_isupper(test_char))
-{
-printf("%c is uppercase.\n", test_char);
-}
-else
-{
-printf("%c is not uppercase.\n", test_char);
-}
+    if (_isupper(test_char))
+    {
+        printf("%c is uppercase.\n", test_char);
+    }
+    else
+    {
+        printf("%c is not uppercase.\n", test_char);
+    }
 
-return 0;
+    return 0;
 }
 
