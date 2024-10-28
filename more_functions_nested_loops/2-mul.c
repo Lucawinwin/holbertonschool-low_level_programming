abf13091af9d7d91eedaa486a4c746b4ef3c8829
root <root@Lucawin.>
2024-10-28 16:00:47 +0100

    la surprise

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,26 +1,11 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * mul - multiplies two integers
- * @a: the first integer
- * @b: the second integer
- *
- * Return: the product of a and b
+ * mul: multiplie 2 entiers
  */
+
 int mul(int a, int b)
 {
 return (a * b);
 }
-
-
-int main(void)
-{
-int num1 = 6;
-int num2 = 7;
-
-int result = mul(num1, num2);
-printf("Le produit de %d et %d est %d.\n", num1, num2, result);
-
-return (0);
-}
-
