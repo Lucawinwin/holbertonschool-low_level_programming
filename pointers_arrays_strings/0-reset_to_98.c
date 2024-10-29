b7eae37826465d61672fa0f7c97191c0d5bda0e7
root <root@Lucawin.>
2024-10-29 16:14:35 +0100

    sef

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void reset_to_98(int *n) {
+    *n = 98;  
+}
+
+int main() {
+    int value = 42;  
+    printf("Avant : %d\n", value); 
+    reset_to_98(&value);  
+    printf("Après : %d\n", value); 
+    return 0;
+}
+
