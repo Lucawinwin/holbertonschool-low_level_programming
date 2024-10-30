b431c0ff92d3d318469a5731fd1bf403e510240f
root <root@Lucawin.>
2024-10-30 09:43:48 +0100

    stand up

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 void reset_to_98(int *n) {
-    *n = 98;  
+*n = 98;
 }
 
 int main() {
-    int value = 42;  
-    printf("Avant : %d\n", value); 
-    reset_to_98(&value);  
-    printf("Après : %d\n", value); 
-    return 0;
+int value = 42;
+printf("Avant : %d\n", value);
+reset_to_98(&value);
+printf("Après : %d\n", value);
+return 0;
 }
