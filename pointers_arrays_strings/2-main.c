79594a304c2ee88ab5277ecceff44f1f67695789
root <root@Lucawin.>
2024-10-30 11:04:01 +0100

    utou

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -2,16 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - check the code .
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *str;
-    int len;
+	char *str;
+	int len;
 
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
-    return (0);
+	str = "Holberton!";
+	len = _strlen(str);
+	printf("%d\n", len);
+	return (0);
+}
