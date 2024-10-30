5a76ba6e4e765bfabdf8aff042d4aa4f67cf88e1
root <root@Lucawin.>
2024-10-30 13:15:07 +0100

    utou

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,31 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * _strlen - Returns the length of a string.
  * @str: The string to get the length of.
  *
- * Return: The length of @str.
+ * Return: The length of @str. If str is NULL, return 0.
  */
 size_t _strlen(const char *str)
 {
-	size_t length = 0;
+    if (str == NULL) {
+        return 0; // Gérer le cas où le pointeur est nul
+    }
 
-	while (*str++)
-		length++;
+    size_t length = 0;
+    while (*str++) {
+        length++;
+    }
 
-	return (length);
+    return length; // Retourner la longueur calculée
 }
+
+int main() {
+    const char *ma_chaine = "Bonjour, monde !";
+    size_t longueur = _strlen(ma_chaine);
+
+    printf("La longueur de la chaîne est : %zu\n", longueur); // Utiliser %zu pour size_t
+    return 0;
+}
+
