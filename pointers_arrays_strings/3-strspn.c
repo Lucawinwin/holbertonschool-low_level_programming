feb11f49e11de2c0cece8b2511bf4a49a0d5efa4
root <root@Lucawin.>
2024-11-04 16:06:26 +0100

    skj

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+int len;
+const char str1[] = "hello world";
+const char str2[] = "oleh";
+
+len = strspn(str1, str2);
+
+printf("Length of initial segment matching %d\n", len);
+
+return (0);
+}
