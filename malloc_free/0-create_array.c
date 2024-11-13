b9f3e52b97402b77c2b38ab3f7debc9bb85b6c2c
root <root@Lucawin.>
2024-11-13 10:10:01 +0100

    10h09

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -25,7 +25,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	array = malloc(sizeof(char) * size);
+	array = malloc(sizeof(char c) * size);
 
 	if (array == NULL)
 		return (NULL);
