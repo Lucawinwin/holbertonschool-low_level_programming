0948100df6515bb91435bcea462806329e2c6281
root <root@Lucawin.>
2024-11-13 10:35:13 +0100

    10h35

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,34 +1,47 @@
-#include <main.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * _strdup - Returns a pointer to a newly-allocated space in memory
- *           containing a copy of the string given as parameter.
- * @str: The string to be copied.
+ * _strdup - returns a pointer to a newly allocated
+ *space in memory, which contains a copy of the
+ *string given as a parameter.
+ *@str:String to be copied
  *
- * Return: If str == NULL or insufficient memory is available - NULL.
- *         Otherwise - a pointer to the duplicated string.
+ * Return: NULL in case of error, pointer to allocated
+ * space
+ *
+ * FUNCTIONALITY
+ *
+ * 1. First, we check if the string is NULL.  If it is, we return NULL.
+ * 2.  Next, we loop through the string to find the length of the string.
+ * 3.  We allocate memory for the copy of the string.
+ * 4.  We check if the memory was allocated.  If it wasn’t, we return NULL.
+ * 5.  We loop through the string again, this time copying the string into the new memory.
+ * 6.  We add a null terminator to the end of the string.
+ * 7.  We return the copy of the string.
+ *
+ * Time Complexity: O(n)
  */
 char *_strdup(char *str)
 {
-	char *duplicate;
-	int index, len = 0;
+	char *cpy;
+	int index, len;
 
 	if (str == NULL)
 		return (NULL);
 
 	for (index = 0; str[index]; index++)
 		len++;
+	cpy = malloc(sizeof(char) * (len + 1));
 
-	duplicate = malloc(sizeof(char) * (len + 1));
-
-	if (duplicate == NULL)
+	if (cpy == NULL)
 		return (NULL);
 
 	for (index = 0; str[index]; index++)
-		duplicate[index] = str[index];
+	{
+		cpy[index] = str[index];
+	}
 
-	duplicate[len] = '\0';
+	cpy[len] = '\0';
 
-	return (duplicate);
+	return (cpy);
 }
