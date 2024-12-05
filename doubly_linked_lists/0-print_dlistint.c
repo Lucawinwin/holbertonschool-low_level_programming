ff62c78e3dc13ab9a55804cd92c548e05d7841bb
Lucawinwin <windwehrluca@gmail.com>
2024-12-05 10:19:25 +0100

    10h19

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,29 @@
-#include"lists.h"
+#include "lists.h"
 
-size_t print_dlistint(const dlistint_t *h);
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
 {
-size_t count = 0;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
+	int counter = 0;
 
-    return count;
+	if (h == NULL)
+		return (counter);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
+	}
+
+	return (counter);
 }
--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,24 @@
-#include"lists.h"
+#include "lists.h"
 
-size_t dlistint_len(const dlistint_t *h);
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
 {
-size_t longueur = 0;
-while (h)
-{
-	longueur++;
-	h = h->next;
-}
-return longueur;
+	int counter = 0;
+
+	if (h == NULL)
+		return (counter);
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+
+	return (counter);
 }
